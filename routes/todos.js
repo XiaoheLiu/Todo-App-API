2c28c8a699495acceed4c7f26b261115a9a0bce2
Athena Liu <sdathena@mail.ubc.ca>
2018-12-11 19:30:47 -0800

    Refactor code by adding helpers

--- routes/todos.js
+++ routes/todos.js
@@ -1,61 +1,21 @@
 var express = require("express"),
     router  = express.Router(),
-    db      = require("../models/index.js");
+    db      = require("../models");
+    helpers = require("../helpers/todos")
 
+
+router.route('/')
 // INDEX
-router.get('/', function(req, res){
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
+    .get(helpers.showTodos)
+// CREATE
+    .post(helpers.createTodo);
 
-// POST
-router.post('/', function(req, res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-
+router.route('/:todoid')
 // SHOW
-router.get('/:todoid', function(req, res){
-    db.Todo.findById(req.params.todoid)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-
-});
-
+    .get(helpers.showTodo)
 // PUT
-router.put('/:todoid', function(req, res){
-    db.Todo.findByIdAndUpdate(req.params.todoid, req.body, {new: true})
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
+    .put(helpers.updateTodo)
 // DELETE
-router.delete('/:todoid', function(req, res){
-    db.Todo.findByIdAndRemove(req.params.todoid)
-    .then(function(){
-        res.json({message: 'Todo is deleted.'});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
